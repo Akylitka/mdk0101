83898fba8b4216ef3978451507714a8852124a14
Akylitka <beauty.dancer.07@gmail.com>
2024-09-17 13:21:34 +0300

    я решила задачу номер 1 вариант 1

--- Skibidi/Program.cs
+++ Skibidi/Program.cs
@@ -11,7 +11,35 @@ namespace Skibidi
         static void Main(string[] args)
         {
             Console.WriteLine("муняняя");
+
+            List<int> list_1 = new List<int>() {1,2,3,4,522,911,420,69, -2, -3};
+
+
+            foreach (int g in list_1)
+            {
+                Console.WriteLine("список до удаления=" + g);
+            }
+
+
+            for (int i = 0; i < list_1.Count; i++)
+            {
+    
+                
+                if (list_1[i] % 2 != 0)
+                {
+                    list_1.RemoveAt(i);
+                }
+     
+            }
+
+           
+            foreach(int g in list_1)
+            {
+                Console.WriteLine("список после удаления =" + g);
+            }
+          
             Console.ReadKey();
+
         }
     }
 }
