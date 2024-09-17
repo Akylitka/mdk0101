a3a70cba660650f7d6684413ce163d443b00b877
Akylitka <beauty.dancer.07@gmail.com>
2024-09-17 12:22:36 +0300

    добавлена муняня

--- Skibidi/Program.cs
+++ Skibidi/Program.cs
@@ -10,7 +10,7 @@ namespace Skibidi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("муняня");
+            Console.WriteLine("муняняя");
             Console.ReadKey();
         }
     }
