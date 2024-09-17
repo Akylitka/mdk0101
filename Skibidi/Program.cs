90c01e031411ba60a84ef5729b3570440bf55448
st56 <st56@WS56-10>
2024-09-17 12:11:45 +0300

    Добавьте файлы проекта.

---
+++ Skibidi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skibidi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
