7972333f486fcfd91d421996d4814c313bc64626
weresk <beresklive@gmail.com>
2024-10-24 13:25:22 +0300

    описала классы, перечисление и мейн для лр2

--- Skibidi/лр2/Program.cs
+++ Skibidi/лр2/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace лр2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Tovar> list = new List<Tovar>
+            {
+             new Tovar("Дробовик", 1500),
+             new Tovar("Пистолет", 800),
+             new Tovar("Автомат", 2500)
+             };
         }
     }
 }
